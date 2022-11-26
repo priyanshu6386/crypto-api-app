b580b5fa6d37713e6d44b24f4c548491b6b22932
priyanshu6386 <priyanshuv624@gmail.com>
2022-11-26 12:32:31 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import Coin from './Components/Coin';
 
 function App() {
   const [coins, setCoins] = useState([]);
-  useEffect(() => {
+   useEffect(() => {
     axios
       .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false' )
       .then(res => {
--- src/Components/Coin.jsx
+++ src/Components/Coin.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './Coin.css';
 
 const Coin = ({ name,price,symbol,marketcap,volume,image, priceChange}) => {
